ce75679eb5d79bc8b8a552d1a3377f2b366848a1
Luan Victor <>
2024-04-16 16:38:18 -0300

    chore: changing api port

--- GymMy-api/src/server.ts
+++ GymMy-api/src/server.ts
@@ -3,6 +3,6 @@ import app from "./app";
 
 dotenv.config();
 
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT ?? 3001;
 
 app.listen(port, () => console.log(`Server online on port ${port}`));
