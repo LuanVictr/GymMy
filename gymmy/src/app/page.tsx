ab98fbc207387e4920ce8896825dc5cbdf9df00f
Luan Victor <>
2024-04-16 17:00:07 -0300

    feat: starting homepage structure

--- gymmy/src/app/page.tsx
+++ gymmy/src/app/page.tsx
@@ -1,11 +1,32 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import { Flex } from 'antd'
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Flex } from "antd";
+import Header from "@/components/header";
 
 export default function Home() {
   return (
-    <Flex>
-      <h1>GymMy</h1>
+    <Flex vertical>
+      <Header />
+      <Flex style={{ width: "100%", height: "100%" }} vertical>
+        <Flex
+          align="center"
+          justify="center"
+          style={{ height: "50%" }}
+          vertical
+        >
+          <Flex vertical align="center" justify="center" style={{width:'80%', textAlign:'center'}}>
+            <h1>
+              Sua própria inteligência artificial para te ajudar nos seus
+              exercícios
+            </h1>
+            <p>Uma ajuda para quando seu instrutor estiver ocupado.</p>
+          </Flex>
+        </Flex>
+
+        <Flex>
+          <p>Testando again</p>
+        </Flex>
+      </Flex>
     </Flex>
-  )
+  );
 }
---
+++ gymmy/src/app/theme.tsx
@@ -0,0 +1,9 @@
+import { ThemeConfig } from "antd";
+
+interface Itheme extends ThemeConfig {
+    colorPrimary: string;
+}
+
+export const theme:Itheme = {
+    colorPrimary: "#F7BC45",
+  };
