a21f0ba9408f945c4c49d037c1b8ba2dc113478e
Luan Victor <>
2024-04-16 16:59:00 -0300

    feat: configuring antd <ConfigProvider> to have a design system primary color

--- gymmy/src/app/layout.tsx
+++ gymmy/src/app/layout.tsx
@@ -1,20 +1,34 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { ConfigProvider } from "antd";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { theme } from "./theme";
 
 export const metadata: Metadata = {
-  title: 'GymMy',
-  description: 'Seu personal inteligente que vai te ajudar no seu progresso.',
-}
+  title: "GymMy",
+  description: "Seu personal inteligente que vai te ajudar no seu progresso.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
+    <ConfigProvider theme={{
+      token: {
+        colorPrimary: theme.colorPrimary,
+      },
+      components: {
+        Button: {
+          colorLink: theme.colorPrimary,
+          colorLinkHover: theme.colorPrimary,
+        }
+      }
+    }}>
+      <html style={{ height: '100%', width: '100%' }} lang="en">
+        <body style={{ height: '100%', margin: 0, padding: 0 }} >{children}</body>
+      </html>
+    </ConfigProvider>
+  );
 }
