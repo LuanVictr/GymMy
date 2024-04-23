5ce9687b82c49b503a2f3cd8c22201b7e118658a
Luan Victor <>
2024-04-23 17:08:25 -0300

    feat: creating User model

---
+++ GymMy-api/src/database/models/user.ts
@@ -0,0 +1,38 @@
+import { DataTypes, Model, Optional, UUIDV4 } from "sequelize";
+import sequelize from "../../infra/connection";
+
+export interface IUser {
+  id: string;
+  name: string;
+  password: string;
+}
+
+interface UserCreationAttributes extends Optional<IUser, 'id'> {}
+
+  class User extends Model<IUser, UserCreationAttributes> implements IUser {
+    public id!: string;
+    public name!: string;
+    public password!: string;
+  }
+
+  User.init(
+    {
+      id: {
+        allowNull: false,
+        type: DataTypes.UUID,
+        primaryKey: true,
+        autoIncrement: false,
+        defaultValue: UUIDV4,
+      },
+      name: DataTypes.STRING,
+      password: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "User",
+      paranoid: true,
+    }
+  );
+
+  export default User;
+
