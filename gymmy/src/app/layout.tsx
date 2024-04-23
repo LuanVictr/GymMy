e257cae94e5567b8baad96d14f70e30320061e0b
Luan Victor <>
2024-04-23 14:28:56 -0300

    feat: adding gymmy logo and changing antd main theme using config provider on layout.tsx

--- gymmy/src/app/layout.tsx
+++ gymmy/src/app/layout.tsx
@@ -1,13 +1,16 @@
 import { ConfigProvider } from "antd";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Quicksand } from "next/font/google";
 import { theme } from "./theme";
 
 export const metadata: Metadata = {
   title: "GymMy",
   description: "Seu personal inteligente que vai te ajudar no seu progresso.",
+  icons:'/gymmy-icon.png'
 };
+
+const quicksand = Quicksand({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
@@ -15,19 +18,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ConfigProvider theme={{
-      token: {
-        colorPrimary: theme.colorPrimary,
-      },
-      components: {
-        Button: {
-          colorLink: theme.colorPrimary,
-          colorLinkHover: theme.colorPrimary,
-        }
-      }
-    }}>
-      <html style={{ height: '100%', width: '100%' }} lang="en">
-        <body style={{ height: '100%', margin: 0, padding: 0 }} >{children}</body>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: theme.colorPrimary,
+        },
+        components: {
+          Button: {
+            colorLink: theme.colorPrimary,
+            colorLinkHover: theme.colorPrimary,
+          },
+        },
+      }}
+    >
+      <html style={{ height: "100%", width: "100%", fontFamily: "" }} lang="en">
+        <body className={quicksand.className} style={{ height: "100%", margin: 0, padding: 0 }}>
+          {children}
+        </body>
       </html>
     </ConfigProvider>
   );
