822dadb35949993945c20cc69954bc8b43d8cd3c
Luan Victor <>
2024-04-23 14:30:03 -0300

    feat: styling main page and header feat: starting route login and register pages

--- gymmy/src/app/page.tsx
+++ gymmy/src/app/page.tsx
@@ -1,30 +1,46 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import { Flex } from "antd";
+"use client"
+
+import { Button, Flex } from "antd";
 import Header from "@/components/header";
+import useMedia from "use-media-antd-query";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  
+  const size = useMedia();
+  const history = useRouter();
+
+  const isMobile = size === 'xs' || size === 'sm';
+
   return (
     <Flex vertical>
       <Header />
       <Flex style={{ width: "100%", height: "100%" }} vertical>
-        <Flex
-          align="center"
-          justify="center"
-          style={{ height: "50%" }}
-          vertical
-        >
-          <Flex vertical align="center" justify="center" style={{width:'80%', textAlign:'center'}}>
-            <h1>
-              Sua própria inteligência artificial para te ajudar nos seus
+        <Flex align="center" justify="center" style={{ height: isMobile ? '60vh' : '80vh',}} vertical>
+          <Flex
+            vertical
+            align="center"
+            justify="center"
+            style={{ width: "90%", textAlign: "center" }}
+          >
+            <h1 style={{fontSize: isMobile ? '2.5rem' : '4.5rem'}}>
+              Sua própria inteligência artificial <br /> para te ajudar <br /> nos seus
               exercícios
             </h1>
-            <p>Uma ajuda para quando seu instrutor estiver ocupado.</p>
+            <p style={{fontSize: '1.125rem'}}>Tire dúvidas enquanto seu instrutor está ocupado.</p>
           </Flex>
+          <Button onClick={() => history.push('/users/register')} shape="round" type="primary">Testar Agora</Button>
         </Flex>
 
-        <Flex>
-          <p>Testando again</p>
+        <Flex align="center" justify="center" style={{ height: "" }} vertical>
+          <Flex
+            vertical
+            align="center"
+            justify="center"
+            style={{ width: "80%", textAlign: "center" }}
+          >
+            <p></p>
+          </Flex>
         </Flex>
       </Flex>
     </Flex>
--- gymmy/src/components/header.tsx
+++ gymmy/src/components/header.tsx
@@ -1,13 +1,17 @@
-import { Button, Flex } from "antd";
+import { Button, Flex, Image } from "antd";
+import { useRouter } from "next/navigation";
 
 function Header() {
+  const history = useRouter();
+
+
   return (
     <Flex align="center" justify="center">
       <Flex style={{ width: "90%" }} align="center" justify="space-between">
-        <h3>GymMy</h3>
+        <Image src="/gymmy-logo.png" width={'120px'} alt="Gymmy logo" preview={false} />
         <Flex>
-          <Button type="link">Entrar</Button>
-          <Button type="primary">Testar Agora</Button>
+          <Button onClick={() => history.push('/users/login')} type="link">Entrar</Button>
+          <Button onClick={() => history.push('/users/register')} shape="round" type="primary">Testar Agora</Button>
         </Flex>
       </Flex>
     </Flex>
