81d000513722f50c37de6cbaa84a09a8e85857aa
Luan Victor <>
2024-04-24 15:53:24 -0300

    feat: creating jsonwebtoken generator and validator

---
+++ GymMy-api/src/utils/jwt/tokenGenerator.ts
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const secret = process.env.JWT_SECRET ?? '3Gk7sR2P8eF';
+
+export function generateToken (payload: {name: string}) {
+  return jwt.sign({name: payload.name}, secret , { expiresIn: '3d'});
+}
+
+export function validateToken (token:string) {
+  return jwt.verify(token, secret, (err, decoded) => {
+    if(err) {
+      return false;
+    }
+    return true;
+  });
+}
