856f532632e5fb314ea274872c35d99b69731d5a
Luan Victor <>
2024-05-01 21:37:02 -0300

    feat: adding cors to server

--- GymMy-api/src/app.ts
+++ GymMy-api/src/app.ts
@@ -3,8 +3,11 @@ import router from './routes';
 import bodyParser from "body-parser";
 import joiError from './infra/handlers/joiError';
 import celebrateError from './infra/handlers/celebrateError';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json({limit: '10mb'}));
 
