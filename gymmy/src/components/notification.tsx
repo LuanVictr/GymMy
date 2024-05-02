f5363fb3fa8f0a4c14522c2dd895d1c16f7df00f
Luan Victor <>
2024-05-01 21:40:14 -0300

    feat: creating notification function

---
+++ gymmy/src/components/notification.tsx
@@ -0,0 +1,29 @@
+import { notification } from 'antd';
+import { FaCircleCheck } from 'react-icons/fa6';
+import { MdCancel } from 'react-icons/md';
+
+export const notificateError = (
+  message = 'Ops, ocorreu um erro ao realizar essa operação. Tente novamente!'
+) => {
+  notification.config({ maxCount: 1 });
+  notification.error({
+    message: message,
+    icon: <MdCancel color="red" size={25} />,
+    placement: 'top',
+    duration: 6,
+    closeIcon: null,
+  });
+};
+
+export const notificateSucess = (
+  message = 'Sucesso ao realizar a operação'
+) => {
+  notification.config({ maxCount: 1 });
+  notification.success({
+    message: message,
+    icon: <FaCircleCheck size={25} color="green" />,
+    placement: 'top',
+    duration: 6,
+    closeIcon: null,
+  });
+};
---
+++ gymmy/src/utils/isMobile.ts
@@ -0,0 +1,5 @@
+function checkIsMobile() {
+  return window.innerWidth < 768;
+}
+
+export default checkIsMobile;
