435882a90e720051dd082fefade5eb428c6b3b96
Luan Victor <>
2024-06-03 15:12:25 -0300

    feat: adding ssl certificate to api

--- GymMy-api/src/server.ts
+++ GymMy-api/src/server.ts
@@ -1,8 +1,16 @@
 import dotenv from "dotenv";
 import app from "./app";
+import https from "https";
+import fs from 'fs';
+import path from "path";
 
 dotenv.config();
 
 const port = process.env.PORT ?? 3001;
 
-app.listen(port, () => console.log(`Server online on port ${port}`));
+const sslServer = https.createServer({
+  key: fs.readFileSync(path.join(__dirname, 'cert', 'cert-key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+}, app);
+
+sslServer.listen(port, () => console.log(`Server online on port ${port}`));
